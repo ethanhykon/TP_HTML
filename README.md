fe4bd05637d12fb25d81eb8d32bc2e4afae0f32b
unknown <saltrennga@gmail.com>
2025-03-20 17:28:21 +0100

    hero banner-footer

---
+++ README.md
@@ -0,0 +1,8 @@
+#Bienvenue sur mon Projet
+
+##Technologie utilisé
+
+tailwind css installé en cli
+Javascript
+html
+npx @tailwindcss/cli -i ./css/style.css -o ./src/output.css --watch
---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// nav button menu 
+const btn = document.getElementById('btn_menu')
+const btn_opn = document.getElementById('btn_opn')
+    const btn_cls = document.getElementById('btn_cls')
+const mb_navbar =document.getElementById('mobile_menu') 
+
+btn.addEventListener('click', function(){
+    
+    console.log('bouton cliqué !');
+    btn_cls.classList.toggle('hidden'); 
+    btn_cls.classList.toggle('block');
+    btn_opn.classList.toggle('hidden');
+    btn_opn.classList.toggle('block'); 
+    
+    console.log(btn_cls);
+    console.log(btn_opn);
+    console.log(mb_navbar);
+
+    
+    if (mb_navbar.classList.contains('hidden')) {
+        mb_navbar.classList.remove('hidden');
+        
+      } else {
+        mb_navbar.classList.add('hidden');
+        
+      } 
+
+})
